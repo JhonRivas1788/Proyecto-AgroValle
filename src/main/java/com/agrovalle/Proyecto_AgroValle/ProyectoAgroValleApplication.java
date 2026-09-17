dc1226740404e5518a497c5270c35c7338444f15
Jhon Rivas <jsrivas@estudiante.uniajc.edu.co>
2026-09-16 19:09:55 -0500

    chore: inicializar estructura base de AgroValle Connect con Maven y Spring Boot

---
+++ src/main/java/com/agrovalle/Proyecto_AgroValle/ProyectoAgroValleApplication.java
@@ -0,0 +1,13 @@
+package com.agrovalle.Proyecto_AgroValle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAgroValleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAgroValleApplication.class, args);
+	}
+
+}
